8f0eff1a6b92ac61c2096bd41b26b6d055b3a70a
pinkeshdarji <pinkesh.earth@gmail.com>
2016-05-14 15:52:57 +0530

    Update socialconfig.py

--- Code/LinkRygbee/linkrygbee/socialconfig.py
+++ Code/LinkRygbee/linkrygbee/socialconfig.py
@@ -8,9 +8,9 @@ CONFIG = {
 
         'class_': oauth2.LinkedIn,
 
-        # Facebook is an AuthorizationProvider too.
-        'consumer_key': '758smstgs08hyg',
-        'consumer_secret': 'sIOvYg3d0Yf21NAt',
+        
+        'consumer_key': 'some_consumer_key',
+        'consumer_secret': 'some_consumer_secret',
 
         # But it is also an OAuth 2.0 provider and it needs scope.
         'scope': ['r_basicprofile','r_emailaddress','rw_company_admin','w_share'],
